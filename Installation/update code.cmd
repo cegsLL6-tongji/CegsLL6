f8d212d9a3011d5100713f0598b9fc054001189a
Jim Wilson <jim.wilson@aeonlaboratories.com>
2024-09-12 08:42:38 -0700

    Altered project settings and 'update code.cmd' to include maximum debugging info with the application. Added VTT.Coldfinger = ftcVTC and set ftcVTC MaximumMinutesToFreeze to 8 as a temporary patch until Section is more VTC/FTC aware.

--- Installation/update code.cmd
+++ Installation/update code.cmd
@@ -4,6 +4,7 @@ C:
 CD "\Programs\Aeon Laboratories\CegsLL6"
 copy "%*\*.exe" > nul
 copy "%*\*.dll" > nul
+copy "%*\*.pdb" > nul
 copy "%*\*.deps.json" > nul
 copy "%*\*.runtimeconfig.json" > nul
 echo *** System software updated *** >> "log\Event log.txt"
